db31281248803d61ad3c61f394d78dfc3a17b1d5
Christian Lorentzen <lorentzen.ch@gmail.com>
2022-12-05 16:22:49 +0100

    ENH add test_plot_bias

--- src/model_diagnostics/calibration/plots.py
+++ src/model_diagnostics/calibration/plots.py
@@ -3,8 +3,11 @@ import typing
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
+import pyarrow as pa
+import pyarrow.compute as pc
 from sklearn.isotonic import IsotonicRegression
 
+from .._utils.array import array_name
 from .identification import compute_bias
 
 
@@ -66,9 +69,117 @@ def plot_reliability_diagram(
     y_obs_min, y_obs_max = np.min(y_obs), np.max(y_obs)
     y_pred_min, y_pred_max = np.min(y_pred), np.max(y_pred)
     iso = IsotonicRegression(y_min=y_obs_min, y_max=y_obs_max).fit(y_pred, y_obs)
+    # diagonal line
     ax.plot([y_pred_min, y_pred_max], [y_pred_min, y_pred_max], "k:")
+    # reliability curve
     ax.plot(iso.X_thresholds_, iso.y_thresholds_)
     ax.set(xlabel="prediction for E(Y|X)", ylabel="estimated E(Y|prediction)")
     ax.set_title("Reliability Diagram")
+
+    return ax
+
+
+def plot_bias(
+    y_obs: npt.ArrayLike,
+    y_pred: npt.ArrayLike,
+    feature: npt.ArrayLike,
+    *,
+    functional: str = "mean",
+    level: float = 0.5,
+    n_bins: int = 10,
+    ax=None,
+):
+    r"""Plot model bias conditional on a feature.
+
+    This plots the generalised bias (residuals), i.e. the values of the canonical
+    identification function, versus a feature. This is a good way to assess whether
+    a model is conditionally calibrated or not. Well calibrated models have bias terms
+    around zero.
+    See Notes for further details.
+
+    Parameters
+    ----------
+    y_obs : array-like of shape (n_obs)
+        Observed values of the response variable.
+        For binary classification, y_obs is expected to be in the interval [0, 1].
+    y_pred : array-like of shape (n_obs)
+        Predicted values of the conditional expectation of Y, :math:`E(Y|X)`.
+    feature : array-like of shape (n_obs)
+        Some feature column.
+    functional : str
+        The functional that is induced by the identification function `V`. Options are:
+        - `"mean"`. Argument `level` is neglected.
+        - `"median"`. Argument `level` is neglected.
+        - `"expectile"`
+        - `"quantile"`
+    level : float
+        The level of the expectile of quantile. (Often called \(\alpha\).)
+        It must be `0 <= level <= 1`.
+        `level=0.5` and `functional="expectile"` gives the mean.
+        `level=0.5` and `functional="quantile"` gives the median.
+    n_bins : int
+        The number of bins for numerical features and the maximal number of (most
+        frequent) categories shown for categorical features.
+    ax : matplotlib.axes.Axes
+        Axes object to draw the plot onto, otherwise uses the current Axes.
+
+    Returns
+    -------
+    ax
+
+    Notes
+    -----
+    A model \(m(X)\) is conditionally calibrated iff \(E(V(m(X), Y))=0\) a.s. The
+    empirical version, given some data, reads \(\frac{1}{n}\sum_i V(m(x_i), y_i)\).
+    This generali. See [FLM2022]`.
+
+    References
+    ----------
+    `FLM2022`
+
+    :   T. Fissler, C. Lorentzen, and M. Mayer.
+        "Model Comparison and Calibration Assessment". (2022)
+        [arxiv:https://arxiv.org/abs/2202.12780](https://arxiv.org/abs/2202.12780).
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    feature_name = array_name(feature, default="feature")
+    df = compute_bias(
+        y_obs=y_obs,
+        y_pred=y_pred,
+        feature=feature,
+        functional=functional,
+        level=level,
+        n_bins=n_bins,
+    )
+
+    is_categorical = False
+    is_string = False
+    if pa.types.is_dictionary(df.column(feature_name).type):
+        is_categorical = True
+    elif pa.types.is_string(df.column(feature_name).type):
+        is_string = True
+
+    # diagonal line
+    if is_categorical or is_string:
+        min_max = {"min": 0, "max": df.shape[0]}
+    else:
+        min_max = pc.min_max(df[feature_name]).as_py()
+    ax.hlines(0, min_max["min"], min_max["max"], color="k", linestyles="dotted")
+    # bias plot
+    # ax.plot(df[feature_name], df["bias_mean"], "o-")
+    ax.errorbar(
+        df[feature_name],
+        df["bias_mean"],
+        yerr=df["bias_stddev"],
+        fmt="o-",
+        capsize=4,
+    )
+    if is_categorical or is_string:
+        ax.set(xlabel=feature_name, ylabel="bias")
+    else:
+        ax.set(xlabel="binned " + feature_name, ylabel="bias")
+    ax.set_title("Bias Plot")
 
     return ax
--- src/model_diagnostics/calibration/tests/test_plots.py
+++ src/model_diagnostics/calibration/tests/test_plots.py
@@ -1,19 +1,56 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pytest
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
-from model_diagnostics.calibration import plot_reliability_diagram
+from model_diagnostics.calibration import plot_bias, plot_reliability_diagram
 
 
-def test_plot_reliability_diagram():
+@pytest.mark.parametrize("ax", [None, plt.subplots()[1]])
+def test_plot_reliability_diagram(ax):
     """Test that plot_reliability_diagram works."""
     X, y = make_classification(random_state=42)
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
     clf = LogisticRegression(solver="lbfgs")
     clf.fit(X_train, y_train)
     y_pred = clf.predict_proba(X_test)[:, 1]
-    ax = plot_reliability_diagram(y_test, y_pred)
+    plt_ax = plot_reliability_diagram(y_obs=y_test, y_pred=y_pred, ax=ax)
 
-    assert ax.get_xlabel() == "prediction for E(Y|X)"
-    assert ax.get_ylabel() == "estimated E(Y|prediction)"
-    assert ax.get_title() == "Reliability Diagram"
+    if ax is not None:
+        assert ax is plt_ax
+    assert plt_ax.get_xlabel() == "prediction for E(Y|X)"
+    assert plt_ax.get_ylabel() == "estimated E(Y|prediction)"
+    assert plt_ax.get_title() == "Reliability Diagram"
+
+
+@pytest.mark.parametrize("ax", [None, plt.subplots()[1]])
+@pytest.mark.parametrize("categorical_type", ["cat", "string"])
+def test_plot_bias(ax, categorical_type):
+    """Test that plot_bias works."""
+    X, y = make_classification(random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    clf = LogisticRegression(solver="lbfgs")
+    clf.fit(X_train, y_train)
+    if categorical_type == "cat":
+        feature = pd.Series(X_test[:, 0].astype("=U8"), dtype="category")
+    elif categorical_type == "string":
+        feature = X_test[:, 0].astype("=U8")
+    else:
+        feature = X_test[:, 0]
+    plt_ax = plot_bias(
+        y_obs=y_test,
+        y_pred=clf.predict_proba(X_test)[:, 1],
+        feature=feature,
+        ax=ax,
+    )
+
+    if ax is not None:
+        assert ax is plt_ax
+    if categorical_type:
+        assert plt_ax.get_xlabel() == "feature"
+    else:
+        assert plt_ax.get_xlabel() == "binned feature"
+    assert plt_ax.get_ylabel() == "bias"
+    assert plt_ax.get_title() == "Bias Plot"
