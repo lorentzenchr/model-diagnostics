BUG set p-value to np.nan for zero variance

src/model_diagnostics/calibration/identification.py
@@ -269,7 +269,7 @@ def compute_bias(
 
         # Add p-value of 2-sided t-test.
         s_ = df.column("bias_stddev").to_numpy()
-        p_value = np.zeros_like(s_)
+        p_value = np.full_like(s_, fill_value=np.nan)
         mask: npt.ArrayLike = s_ > 0
         x = df.column("bias_mean").to_numpy()[mask]
         n = df.column("bias_count").to_numpy()[mask]

src/model_diagnostics/calibration/tests/test_identification.py
@@ -212,7 +212,7 @@ def test_compute_bias_multiple_predictions():
             "bias_mean": [0.0, -1, 2, 1],
             "bias_count": [5] * 4,
             "bias_stddev": [0.0] * 4,
-            "p_value": [0.0] * 4,
+            "p_value": [np.nan] * 4,
         }
     )
     pd.testing.assert_frame_equal(df_bias.to_pandas(), df_expected)