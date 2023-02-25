2a46b637f09f8e5629434f07a9ad694d5f196de9
Christian Lorentzen <lorentzen.ch@gmail.com>
2023-02-25 19:29:27 +0100

    ENH add HomogeneousQuantileScore

--- src/model_diagnostics/scoring/tests/test_scoring.py
+++ src/model_diagnostics/scoring/tests/test_scoring.py
@@ -6,32 +6,58 @@ import pytest
 from model_diagnostics.scoring import (
     GammaDeviance,
     HomogeneousExpectileScore,
+    HomogeneousQuantileScore,
+    PinballLoss,
     PoissonDeviance,
     SquaredError,
 )
 
+HES = HomogeneousExpectileScore
+HQS = HomogeneousQuantileScore
+
 SCORES = [
-    HomogeneousExpectileScore(degree=-1, level=0.2),
-    HomogeneousExpectileScore(degree=-1, level=0.5),
-    HomogeneousExpectileScore(degree=-1, level=0.99),
-    HomogeneousExpectileScore(degree=0, level=0.2),
-    HomogeneousExpectileScore(degree=0, level=0.5),
-    HomogeneousExpectileScore(degree=0, level=0.99),
-    HomogeneousExpectileScore(degree=0.5, level=0.2),
-    HomogeneousExpectileScore(degree=0.5, level=0.5),
-    HomogeneousExpectileScore(degree=0.5, level=0.99),
-    HomogeneousExpectileScore(degree=1, level=0.2),
-    HomogeneousExpectileScore(degree=1, level=0.5),
-    HomogeneousExpectileScore(degree=1, level=0.99),
-    HomogeneousExpectileScore(degree=1.5, level=0.2),
-    HomogeneousExpectileScore(degree=1.5, level=0.5),
-    HomogeneousExpectileScore(degree=1.5, level=0.99),
-    HomogeneousExpectileScore(degree=2, level=0.2),
-    HomogeneousExpectileScore(degree=2, level=0.5),
-    HomogeneousExpectileScore(degree=2, level=0.99),
-    HomogeneousExpectileScore(degree=2.5, level=0.2),
-    HomogeneousExpectileScore(degree=2.5, level=0.5),
-    HomogeneousExpectileScore(degree=2.5, level=0.99),
+    HES(degree=-1, level=0.2),
+    HES(degree=-1, level=0.5),
+    HES(degree=-1, level=0.99),
+    HES(degree=0, level=0.2),
+    HES(degree=0, level=0.5),
+    HES(degree=0, level=0.99),
+    HES(degree=0.5, level=0.2),
+    HES(degree=0.5, level=0.5),
+    HES(degree=0.5, level=0.99),
+    HES(degree=1, level=0.2),
+    HES(degree=1, level=0.5),
+    HES(degree=1, level=0.99),
+    HES(degree=1.5, level=0.2),
+    HES(degree=1.5, level=0.5),
+    HES(degree=1.5, level=0.99),
+    HES(degree=2, level=0.2),
+    HES(degree=2, level=0.5),
+    HES(degree=2, level=0.99),
+    HES(degree=2.5, level=0.2),
+    HES(degree=2.5, level=0.5),
+    HES(degree=2.5, level=0.99),
+    HQS(degree=-1, level=0.2),
+    HQS(degree=-1, level=0.5),
+    HQS(degree=-1, level=0.99),
+    HQS(degree=0, level=0.2),
+    HQS(degree=0, level=0.5),
+    HQS(degree=0, level=0.99),
+    HQS(degree=0.5, level=0.2),
+    HQS(degree=0.5, level=0.5),
+    HQS(degree=0.5, level=0.99),
+    HQS(degree=1, level=0.2),
+    HQS(degree=1, level=0.5),
+    HQS(degree=1, level=0.99),
+    HQS(degree=1.5, level=0.2),
+    HQS(degree=1.5, level=0.5),
+    HQS(degree=1.5, level=0.99),
+    HQS(degree=2, level=0.2),
+    HQS(degree=2, level=0.5),
+    HQS(degree=2, level=0.99),
+    HQS(degree=3, level=0.2),
+    HQS(degree=3, level=0.5),
+    HQS(degree=3, level=0.99),
 ]
 
 
@@ -41,15 +67,20 @@ def sf_name(sf):
         return f"HomogeneousExpectileScore(degree={sf.degree}, level={sf.level})"
     elif isclass(sf):
         return sf.__class__.__name__
-    else:
+    elif hasattr(sf, "__name__"):
         return sf.__name__
+    else:
+        return sf
 
 
+@pytest.mark.parametrize("sf", [HES, HQS])
 @pytest.mark.parametrize(
-    "sf, level, msg",
+    "level, msg",
     [
-        (HomogeneousExpectileScore, -1, "Argument level must fulfil 0 <= level <= 1"),
-        (HomogeneousExpectileScore, 1.1, "Argument level must fulfil 0 <= level <= 1"),
+        (-1, "Argument level must fulfil 0 < level < 1"),
+        (0, "Argument level must fulfil 0 < level < 1"),
+        (1, "Argument level must fulfil 0 < level < 1"),
+        (1.1, "Argument level must fulfil 0 < level < 1"),
     ],
 )
 def test_scoring_function_raises(sf, level, msg):
@@ -58,8 +89,40 @@ def test_scoring_function_raises(sf, level, msg):
         sf(level=level)
 
 
+@pytest.mark.parametrize(
+    "sf, y_obs, y_pred, msg",
+    [
+        (HES(degree=1.1), -1, -2, None),
+        (HES(degree=1), 0, 1, None),
+        (HES(degree=1), 1, 0, "Valid domain .* y_obs >= 0 and y_pred > 0"),
+        (HES(degree=0.5), 0, 1, None),
+        (HES(degree=0.5), 1, 0, "Valid domain .* y_obs >= 0 and y_pred > 0"),
+        (HES(degree=0), 1, 1, None),
+        (HES(degree=0), 0, 1, "Valid domain .* y_obs > 0 and y_pred > 0"),
+        (HES(degree=0), 1, 0, "Valid domain .* y_obs > 0 and y_pred > 0"),
+        (HES(degree=-1), 1, 1, None),
+        (HES(degree=-1), 0, 1, "Valid domain .* y_obs > 0 and y_pred > 0"),
+        (HES(degree=-2), 1, 0, "Valid domain .* y_obs > 0 and y_pred > 0"),
+        (HQS(degree=3), -1, -1, None),
+        (HQS(degree=1), -1, -1, None),
+        (HQS(degree=1.1), 0, 1, "Valid domain .* y_obs > 0 and y_pred > 0"),
+        (HQS(degree=1.1), 1, 0, "Valid domain .* y_obs > 0 and y_pred > 0"),
+        (HQS(degree=0), 0, 1, "Valid domain .* y_obs > 0 and y_pred > 0"),
+        (HQS(degree=0), 1, 0, "Valid domain .* y_obs > 0 and y_pred > 0"),
+    ]
+)
+def test_scoring_function_domain(sf, y_obs, y_pred, msg):
+    """Test the valid domains of scoring functions."""
+    if msg is None:
+        # no error
+        sf(y_obs=y_obs, y_pred=y_pred)
+    else:
+        with pytest.raises(ValueError, match=msg):
+            sf(y_obs=y_obs, y_pred=y_pred)
+
+
 @pytest.mark.parametrize("sf", SCORES, ids=sf_name)
-def test_scoring_function_equal_input(sf):
+def test_scoring_function_for_equal_input(sf):
     """Test that S(x, x) is zero."""
     rng = np.random.default_rng(112358132134)
     n = 5
@@ -126,10 +189,22 @@ def test_homogeneous_scoring_function_against_precomputed_values():
     assert sf(y_obs=y_obs, y_pred=y_pred) == pytest.approx(precomputed)
     assert gd(y_obs=y_obs, y_pred=y_pred) == pytest.approx(precomputed)
 
+    sf = HomogeneousQuantileScore(degree=1, level=0.5)
+    pl = PinballLoss(level=0.5)
+    precomputed = (3 / 2 + 1 / 2) / 2
+    assert sf(y_obs=y_obs, y_pred=y_pred) == pytest.approx(precomputed)
+    assert pl(y_obs=y_obs, y_pred=y_pred) == pytest.approx(precomputed)
+
+    sf = HomogeneousQuantileScore(degree=2, level=1 / 4)
+    precomputed = (3/4 * 15 / 2 + 1/4 * 3 / 2) / 2
+    assert sf(y_obs=y_obs, y_pred=y_pred) == pytest.approx(precomputed)
+
 
 def test_scoring_function_functional():
-    """Test that HomogeneousExpectileScore returns the right functional."""
+    """Test that scoring function returns the right functional."""
     sf = HomogeneousExpectileScore(level=0.5)
     assert sf.functional == "mean"
     sf = HomogeneousExpectileScore(level=0.5001)
     assert sf.functional == "expectile"
+    sf = HomogeneousQuantileScore()
+    assert sf.functional == "quantile"
