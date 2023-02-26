ff7014076b22cc0f750f8ebf8feaf2d5733c5b07
Christian Lorentzen <lorentzen.ch@gmail.com>
2023-02-26 10:41:05 +0100

    MNT expectile from scipy >= 1.10

--- src/model_diagnostics/calibration/tests/test_identification.py
+++ src/model_diagnostics/calibration/tests/test_identification.py
@@ -6,49 +6,9 @@ import polars as pl
 import pyarrow as pa
 import pytest
 from polars.testing import assert_frame_equal
-from scipy.optimize import root_scalar
-from scipy.stats import ttest_1samp
+from scipy.stats import expectile, ttest_1samp
 
 from model_diagnostics.calibration import compute_bias, identification_function
-
-
-# Note: expectile might arrive with scipy 1.10.
-# Backport of scipy.stats.expectile
-# https://github.com/scipy/scipy/pull/17039
-def expectile(a, alpha=0.5, *, dtype=None, weights=None):  # pragma: no cover
-    r"""Compute the expectile along the specified axis."""
-    if alpha < 0 or alpha > 1:
-        raise ValueError("The expectile level alpha must be in the range [0, 1].")
-    a = np.asarray(a, dtype=dtype)
-
-    if weights is not None:
-        weights = np.asarray(weights, dtype=dtype)
-
-    # This is the empirical equivalent of Eq. (13) with identification
-    # function from Table 9 (omitting a factor of 2) in
-    # Gneiting, T. (2009). "Making and Evaluating Point Forecasts".
-    # Journal of the American Statistical Association, 106, 746 - 762.
-    # https://doi.org/10.48550/arXiv.0912.0902
-    def first_order(t):
-        return alpha * np.average(np.fmax(0, a - t), weights=weights) - (
-            1 - alpha
-        ) * np.average(np.fmax(0, t - a), weights=weights)
-
-    if alpha >= 0.5:
-        x0 = np.average(a, weights=weights)
-        x1 = np.amax(a)
-    else:
-        x1 = np.average(a, weights=weights)
-        x0 = np.amin(a)
-
-    if x0 == x1:
-        # a has a single unique element
-        return x0
-
-    # Note that the expectile is the unique solution, so no worries about
-    # finding a wrong root.
-    res = root_scalar(first_order, x0=x0, x1=x1)
-    return res.root
 
 
 @pytest.mark.parametrize(
